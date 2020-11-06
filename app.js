04719a5c39e52add417b98b5a5087ad7aefa5487
analiavargas <vargas_va@live.com.ar>
2020-11-06 16:40:13 -0300

    Subo correcion de app.js

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ app.get ('/', function (req, res) {
 	res.sendFile(__dirname + '/views/index.html')
 })
 
+app.get ('/ayuda', function (req, res) {
+	res.sendFile(__dirname + '/views/ayuda.html')
+})
+
+app.get ('/creaTuCuenta', function (req, res) {
+	res.sendFile(__dirname + '/views/creaTuCuenta.html')
+})
+
+app.get ('/ingresa', function (req, res) {
+	res.sendFile(__dirname + '/views/ingresa.html')
+})
+
+app.get ('/misCompras', function (req, res) {
+	res.sendFile(__dirname + '/views/misCompras.html')
+})
+
+app.get ('/ofertas', function (req, res) {
+	res.sendFile(__dirname + '/views/ofertas.html')
+})
+
+app.get ('/tiendasOficiales', function (req, res) {
+	res.sendFile(__dirname + '/views/tiendasOficiales.html')
+})
+
+app.get ('/vender', function (req, res) {
+	res.sendFile(__dirname + '/views/vender.html')
+})
+
 app.get ('*', function (req, res) {
 	res.sendFile(__dirname + '/public/' + req.url)
 })
